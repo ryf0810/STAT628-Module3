c854ef465d4c09a5de73f7b5e737642544826845
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-12-05 17:01:09 -0600

    EDA analysis

--- code/EDA.R
+++ code/EDA.R
@@ -1,12 +1,11 @@
+library(gplots)
 hotel_data <- read.csv("hotel_SB_county.csv")
 filter_df1 <- hotel_data[,c("business_id","review_count","useful","stars_review","text")]
 filter_df1$comment_length <- nchar(filter_df1$text)
 correlation <- cor(filter_df1$comment_length, filter_df1$stars_review)
 filter_df1$WordCount <- sapply(strsplit(filter_df1$text, "\\s+"), length)
 filter_df1$CharCount <- nchar(filter_df1$text)
-filter_df1$AverageWordLength <- filter_df1$CharCount / filter_df1$WordCount
-cor_matrix <- cor(filter_df1[,c("stars_review","comment_length","AverageWordLength")])
-heatmap.2(cor_matrix, 
-          trace = "none", 
-          col = colorRampPalette(c("blue", "white", "red"))(100), 
-          main = 'Correlation Matrix')
+filter_df1$Ave <- filter_df1$CharCount / filter_df1$WordCount
+cor_matrix <- cor(filter_df1[,c("stars_review","comment_length","Ave")])
+
+write.csv(filter_df1,"EDA.csv")
